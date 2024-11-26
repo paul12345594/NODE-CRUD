f7d4bf3fcc5dd21a48617b4142920135ef18ff55
unknown <dungcapaul6@gmail.com>
2024-11-26 21:35:11 +0800

    Reviewing

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
-const users = [];  // FOR ADDING ID AND ETC...
-//let users = []      // FOR DELETING  THE ID 
+//const users = [];  // FOR ADDING ID AND ETC...
+let users = []      // FOR DELETING  THE ID 
 
 export const getUsers = (req,res) => {
     console.log(users);
@@ -16,22 +16,22 @@ export const createUser = (req, res) => {
 
 export const getUser = (req, res) => {
     const { id } = req.params;
-    const foundUser =  users.find((user) => user.id = id);
+    const foundUser =  users.find((user) => user.id === id);
     res.send(foundUser);
 }
 
 export const deleteUser = (req, res) => {
     const { id } = req.params;
 
-    users = users.filter((user) => user.id != id);
-    res.send(`User with the id ${id} deleted from the database. `);
+    users = users.filter((user) => user.id !== id);
+    res.send(`User with the id ${id} deleted from the database.`);
 }
 
 export const updateUser = (req,res) => {
     const { id } = req.params;    
     const { firstName, lastName, age } = req.body;
 
-    const user = users.find((user) => user.id == id );
+    const user = users.find((user) => user.id === id );   // MODIFIED  from ==  => ===
 
     if(firstName) user.firstName = firstName
     if(lastName) user.lastName = lastName
